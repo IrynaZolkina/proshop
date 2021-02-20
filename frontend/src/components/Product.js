edb062b186630859cfb9859a17fafb09827a2b27
IrynaZolkina <54338063+IrynaZolkina@users.noreply.github.com>
2021-02-19 17:39:47 -0800

    chapter 3 completed

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import products from "../products";
+//import products from "../products";
 import Rating from "./Rating";
 
 const Product = ({ product }) => {
